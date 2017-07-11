6978a3602b9b0087718d5b3af7664c072080c342
C4psL0cks <fenixalone@hotmail.com>
2017-07-11 15:21:07 +0700

    rr

--- bot.php
+++ bot.php
@@ -48,51 +48,14 @@ elseif (is_null($events['events'])) {
     // Reply only when message sent is in 'text' format
     if ($event['type'] == 'message' && $event['message']['type'] == 'image'){
       // Get text sent
-      $text = $event['message']['id'];
+      $image = $event['message']['id'];
 
       // Get replyToken
       $replyToken = $event['replyToken'];
       // Build message to reply back
       $messages = [
         'type' => 'id',
-        'id' => $text
-      ];
-      // Make a POST Request to Messaging API to reply to sender
-      $url = 'https://api.line.me/v2/bot/message/reply';
-      $data = [
-        'replyToken' => $replyToken,
-        'messages' => [$messages],
-      ];
-      $post = json_encode($data);
-      $headers = array('Content-Type: application/json', 'Authorization: Bearer ' . $access_token);
-
-      $ch = curl_init($url);
-      curl_setopt($ch, CURLOPT_CUSTOMREQUEST, "POST");
-      curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
-      curl_setopt($ch, CURLOPT_POSTFIELDS, $post);
-      curl_setopt($ch, CURLOPT_HTTPHEADER, $headers);
-      curl_setopt($ch, CURLOPT_FOLLOWLOCATION, 1);
-      $result = curl_exec($ch);
-      curl_close($ch);
-
-      echo $result . "\r\n";
-    }
-  }
-
-}
-elseif (is_null($events['events'])) {
-  foreach ($events['events'] as $event) {
-    // Reply only when message sent is in 'text' format
-    if ($event['type'] == 'message' && $event['message']['type'] == 'sticker'){
-      // Get text sent
-      $text = $event['message']['stickerId'];
-
-      // Get replyToken
-      $replyToken = $event['replyToken'];
-      // Build message to reply back
-      $messages = [
-        'type' => 'stickerId',
-        'stickerId' => $text
+        'id' => $image
       ];
       // Make a POST Request to Messaging API to reply to sender
       $url = 'https://api.line.me/v2/bot/message/reply';
