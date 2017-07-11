9e5af389b2e43697932aea9d836b7589c148d53b
C4psL0cks <fenixalone@hotmail.com>
2017-07-11 14:42:27 +0700

    up

--- bot.php
+++ bot.php
@@ -10,56 +10,31 @@ if (!is_null($events['events'])) {
   // Loop through each event
   foreach ($events['events'] as $event) {
     // Reply only when message sent is in 'text' format
-    if ($event['type'] == 'message' && $event['message']['type'] == 'text') {
+    if ($event['type'] == 'message' && $event['message']['type'] == 'text'){
       // Get text sent
       $text = $event['message']['text'];
 
-      switch ($text) {
-        case 'สวัสดี':
-          # code...
-          if($text == "สวัสดีครับ" or $text == "ดีครับ" or $text == "ดี")
-          {
-            $text = "สวัสดี ค่ะ มีอะไรให้รับใช้หรอคะ :)";
-          }
-          break;
-        
-        default:
-          # code...
-          break;
-      }
-
-     /* if($text == "สวัสดีครับ" or $text == "ดีครับ" or $text == "ดี")
-      {
+      if($text == "สวัสดีครับ" or $text == "สวัสดีค่ะ" or $text == "ดีครับ" or $text == "ดีค่ะ" or $text == "ดี" ){
         $text = "สวัสดี ค่ะ มีอะไรให้รับใช้หรอคะ :)";
       }
-      elseif ($text == "ป่าว" or $text == "เปล่า" or $text == "ไม่มีอะไร") 
-      {
+      elseif ($text == "ป่าว" or $text == "เปล่า" or $text == "ไม่มีอะไร"){
         $text = "ยินดีที่ได้รับใช้ค่ะ :)";
       }
-      elseif ($text == "ควย" or $text == "พ่อง" or $text == "อีสัส" or $text == "เหี้ย") 
-      {
-        $text = "คุณพูดไม่เพราะเลยนะค่ะ ".$text."";
-      }
-      elseif ($text == "ชื่อไร" or $text == "มีชื่อไหม" or $text == "ชื่อ") 
-      {
+      elseif ($text == "ชื่อไร" or $text == "มีชื่อไหม" or $text == "ชื่อ") {
         $text = "ฉันชื่อ Mark I ฉันถูกสร้างโดย mr.capsLock ค่ะ ";
       }
-      elseif ($text == "ทำไรได้บ้าง" or $text == "ทำไรได้" or $text == "ทำไรเป็น") 
-      {
+      elseif ($text == "ทำไรได้บ้าง" or $text == "ทำไรได้" or $text == "ทำไรเป็น"){
         $text = "ตอนนี้ mr.capsLock กำลังพัฒนาให้ ฉันมีความสามารถตอบโต้กลับได้อย่างเดียวค่ะ";
       }
-      elseif ($text == "ขอบคุณ" or $text == "แต็งกิว" or $text == "ขอบใจ") 
-      {
+      elseif ($text == "ขอบคุณ" or $text == "แต็งกิว" or $text == "ขอบใจ"){
         $text = "ยินดีค่ะ ค่ะ";
       }
-      elseif ($text == "ครับ" or $text == "ค่ะ" or $text == "คะ"  or $text == "อ่า"  or $text == "อื้ม"  or $text == "จ้ะ"  or $text == "จ่ะ") 
-      {
+      elseif ($text == "ครับ" or $text == "ค่ะ" or $text == "คะ"  or $text == "อ่า"  or $text == "อื้ม"  or $text == "จ้ะ"  or $text == "จ่ะ"){
         $text = "ค่ะ :)";
       }
-      else
-      {
+      else{
         $text = "ขอเวลาเรียนรู้ก่อนนะค่ะ ฉันยังไม่เข้าใจ";
-      }*/
+      }
 
       // Get replyToken
       $replyToken = $event['replyToken'];
@@ -89,5 +64,79 @@ if (!is_null($events['events'])) {
       echo $result . "\r\n";
     }
   }
+}
+elseif (is_null($events['events'])) {
+  foreach ($events['events'] as $event) {
+    // Reply only when message sent is in 'text' format
+    if ($event['type'] == 'image' && $event['image']['type'] == 'number'){
+      // Get text sent
+      $text = $event['image']['number'];
+
+      // Get replyToken
+      $replyToken = $event['replyToken'];
+      // Build message to reply back
+      $messages = [
+        'type' => 'number',
+        'number' => $text
+      ];
+      // Make a POST Request to Messaging API to reply to sender
+      $url = 'https://api.line.me/v2/bot/message/reply';
+      $data = [
+        'replyToken' => $replyToken,
+        'number' => [$messages],
+      ];
+      $post = json_encode($data);
+      $headers = array('Content-Type: application/json', 'Authorization: Bearer ' . $access_token);
+
+      $ch = curl_init($url);
+      curl_setopt($ch, CURLOPT_CUSTOMREQUEST, "POST");
+      curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+      curl_setopt($ch, CURLOPT_POSTFIELDS, $post);
+      curl_setopt($ch, CURLOPT_HTTPHEADER, $headers);
+      curl_setopt($ch, CURLOPT_FOLLOWLOCATION, 1);
+      $result = curl_exec($ch);
+      curl_close($ch);
+
+      echo $result . "\r\n";
+    }
+  }
+
+}
+elseif (is_null($events['events'])) {
+  foreach ($events['events'] as $event) {
+    // Reply only when message sent is in 'text' format
+    if ($event['type'] == 'sticker' && $event['sticker']['type'] == 'number'){
+      // Get text sent
+      $text = $event['sticker']['number'];
+
+      // Get replyToken
+      $replyToken = $event['replyToken'];
+      // Build message to reply back
+      $messages = [
+        'type' => 'number',
+        'number' => $text
+      ];
+      // Make a POST Request to Messaging API to reply to sender
+      $url = 'https://api.line.me/v2/bot/message/reply';
+      $data = [
+        'replyToken' => $replyToken,
+        'sticker' => [$messages],
+      ];
+      $post = json_encode($data);
+      $headers = array('Content-Type: application/json', 'Authorization: Bearer ' . $access_token);
+
+      $ch = curl_init($url);
+      curl_setopt($ch, CURLOPT_CUSTOMREQUEST, "POST");
+      curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+      curl_setopt($ch, CURLOPT_POSTFIELDS, $post);
+      curl_setopt($ch, CURLOPT_HTTPHEADER, $headers);
+      curl_setopt($ch, CURLOPT_FOLLOWLOCATION, 1);
+      $result = curl_exec($ch);
+      curl_close($ch);
+
+      echo $result . "\r\n";
+    }
+  }
+
 }
 echo "OK";
