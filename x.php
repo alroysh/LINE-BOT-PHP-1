1d38b9cd7c69d702fef2ea5f8e875fef1ca92e68
C4psL0cks <fenixalone@hotmail.com>
2017-07-13 09:32:20 +0700

    up

--- x.php
+++ x.php
@@ -1,5 +1,5 @@
 <?php
-		$content1 = file_get_contents('http://203.151.143.172/Json/gen_json1.php');
-        $events1 = json_decode($content1, true);
-        echo $events1['tempC'];
+	$content1 = file_get_contents('http://203.151.143.172/Json/gen_json1.php');
+    $events1 = json_decode($content1, true);
+    echo $events1['tempC'];
 ?>
