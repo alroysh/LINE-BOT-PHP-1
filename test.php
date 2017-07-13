27bfc8155804cffb142168b62b7a24ca8673ffaf
C4psL0cks <fenixalone@hotmail.com>
2017-07-13 16:23:56 +0700

    up

---
+++ test.php
@@ -0,0 +1,12 @@
+<?php 
+
+$con = file_get_contents('http://203.151.143.172/Json/gen_json1.php');
+$even = json_decode($con, true);
+
+  if(!is_null($events))
+  {
+    print_r($events); 
+  }
+
+
+ ?>
