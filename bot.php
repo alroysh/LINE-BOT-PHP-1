814c7349be7e0ae3bd5b98bc42df692f4e8b0dc6
C4psL0cks <fenixalone@hotmail.com>
2017-07-13 11:10:10 +0700

    up

--- bot.php
+++ bot.php
@@ -12,7 +12,7 @@ if(isset($events)){
 
 $headers = array('Content-Type: application/json', 'Authorization: Bearer ' . $access_token);
 
-$ch = curl_init($url);
+/*$ch = curl_init($url);
 curl_setopt($ch, CURLOPT_CUSTOMREQUEST, "POST");
 curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
 curl_setopt($ch, CURLOPT_POSTFIELDS, $post);
@@ -21,7 +21,7 @@ curl_setopt($ch, CURLOPT_FOLLOWLOCATION, 1);
 $result = curl_exec($ch);
 curl_close($ch);
 
-echo $result;
+echo $result;*/
 
 
 /*if (!is_null($events['events'])) {
