01e926e677757521c62769ca3ad6416d52affa83
C4psL0cks <fenixalone@hotmail.com>
2017-07-13 16:42:58 +0700

    up

--- test.php
+++ test.php
@@ -2,11 +2,14 @@
 	header("Content-type:application/json; charset=UTF-8");    
     header("Cache-Control: no-store, no-cache, must-revalidate");         
     header("Cache-Control: post-check=0, pre-check=0", false); 
-	$con = file_get_contents('http://203.151.143.172/Json/gen_json1.php');
-	$even = json_decode($con, true);
+	$content = file_get_contents('http://203.151.143.172/Json/gen_json1.php');
+    $result = json_decode($content, true);
+    
+    //print_r($result);
 
-	  if(!is_null($events))
-	  {
-	    print_r($events); 
-	  }
+    if(isset($result)){  
+        $json = json_encode($result); 
+        echo $json;
+    }
+     
  ?>
