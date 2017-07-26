ce9a3000842e56891f40eaa1f8ac1f0543e6688b
Esc <fenixalone@hotmail.com>
2017-07-26 23:10:12 +0700

    up

--- bot.php
+++ bot.php
@@ -5,7 +5,7 @@ $access_token = '3YxSOfQKva9QC3/swCvMwJwJkdnmbiENnLvM5Qf1tF78RW2z5MZGrNnvH+CapO9
 $content = file_get_contents('php://input');
 $events = json_decode($content, true);
 
-$database = file_get_contents('http://949b3eea.ngrok.io/code/node/changdatatojsongraph.php');
+$database = file_get_contents('http://949b3eea.ngrok.io/code/node/jsontoline.php');
 $gen = json_decode($database, true);
 
 if(isset($gen))
