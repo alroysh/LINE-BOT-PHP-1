936450b4716177abfcd2390ffca6524d565dda3e
Esc <fenixalone@hotmail.com>
2017-07-26 22:59:31 +0700

    up

--- bot.php
+++ bot.php
@@ -5,12 +5,18 @@ $access_token = '3YxSOfQKva9QC3/swCvMwJwJkdnmbiENnLvM5Qf1tF78RW2z5MZGrNnvH+CapO9
 $content = file_get_contents('php://input');
 $events = json_decode($content, true);
 
+if(isset($events))
+{
+  print_r($events); 
+}
+
+
 $contents = file_get_contents('http://949b3eea.ngrok.io/code/node/changdatatojsongraph.php');
-$results = json_encode($contents);
+$results = json_decode($contents, true);
 
 if(isset($results))
 {
-  echo $results; 
+  print_r($results); 
 }
 
 if (!is_null($events['events'])) {
