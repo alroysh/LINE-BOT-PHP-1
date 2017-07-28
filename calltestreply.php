c89045bcbeb7a38ab1e2d65a6b53abe0c733d6ad
Esc <fenixalone@hotmail.com>
2017-07-28 15:34:04 +0700

    up

--- calltestreply.php
+++ calltestreply.php
@@ -1,8 +1,22 @@
 <?php
   $access_token = '3YxSOfQKva9QC3/swCvMwJwJkdnmbiENnLvM5Qf1tF78RW2z5MZGrNnvH+CapO9xmv9uYCdUUpYuo/MtK5hyYYTlIBVfBxBzhRxMFQwSjb/EqYvnqU2ZkJt2r3n/2+fcLspZqwyf0TJ7EdYGr8TwwAdB04t89/1O/w1cDnyilFU=';
 
-  $userId ='U00e6d214ca004d0cc011f7924abd6a13';
-  
+  $content = file_get_contents('php://input');
+  $events = json_decode($content, true);
+
+ if (!is_null($events['events'])) {
+
+  foreach ($events['events'] as $event) {
+
+    if ($event['type'] == 'message' && $event['message']['type'] == 'text'){
+
+        $userId = $event['message']['source']['userId'];
+      }
+    }
+  }
+
+  //$userId ='U00e6d214ca004d0cc011f7924abd6a13';
+
   function getname($name,$access_token,$userId){
 
     $url = 'https://api.line.me/v2/bot/profile/'.$userId;
