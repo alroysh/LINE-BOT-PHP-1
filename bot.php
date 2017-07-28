acb2a7358f340d556b18011a56272a151363b762
Esc <fenixalone@hotmail.com>
2017-07-28 14:01:10 +0700

    up

--- bot.php
+++ bot.php
@@ -1,19 +1,25 @@
 <?php
 $access_token = '3YxSOfQKva9QC3/swCvMwJwJkdnmbiENnLvM5Qf1tF78RW2z5MZGrNnvH+CapO9xmv9uYCdUUpYuo/MtK5hyYYTlIBVfBxBzhRxMFQwSjb/EqYvnqU2ZkJt2r3n/2+fcLspZqwyf0TJ7EdYGr8TwwAdB04t89/1O/w1cDnyilFU=';
 
+function getname($name)
+{
+  $url = 'https://api.line.me/v2/bot/profile/U00e6d214ca004d0cc011f7924abd6a13';
 
-$url = 'https://api.line.me/v2/bot/profile/U00e6d214ca004d0cc011f7924abd6a13';
-
-$headers = array('Authorization: Bearer ' . $access_token);
+  $headers = array('Authorization: Bearer ' . $access_token);
 
 
-$ch = curl_init($url);
-curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
-curl_setopt($ch, CURLOPT_HTTPHEADER, $headers);
-curl_setopt($ch, CURLOPT_FOLLOWLOCATION, 1);
-$result = curl_exec($ch);
-curl_close($ch);
+  $ch = curl_init($url);
+  curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+  curl_setopt($ch, CURLOPT_HTTPHEADER, $headers);
+  curl_setopt($ch, CURLOPT_FOLLOWLOCATION, 1);
+  $result = curl_exec($ch);
+  curl_close($ch);
 
+  return $result;
+}
+
+
+$result = getname($name);
 echo $result;
 
 
