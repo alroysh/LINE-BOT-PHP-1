d94cbac8165b65482b6b8d7300b03ab31192ee50
Esc <fenixalone@hotmail.com>
2017-07-28 14:03:57 +0700

    up

--- bot.php
+++ bot.php
@@ -1,7 +1,7 @@
 <?php
 $access_token = '3YxSOfQKva9QC3/swCvMwJwJkdnmbiENnLvM5Qf1tF78RW2z5MZGrNnvH+CapO9xmv9uYCdUUpYuo/MtK5hyYYTlIBVfBxBzhRxMFQwSjb/EqYvnqU2ZkJt2r3n/2+fcLspZqwyf0TJ7EdYGr8TwwAdB04t89/1O/w1cDnyilFU=';
 
-function getname($name)
+function getname($name,$access_token)
 {
   $url = 'https://api.line.me/v2/bot/profile/U00e6d214ca004d0cc011f7924abd6a13';
 
@@ -19,7 +19,7 @@ function getname($name)
 }
 
 
-$result = getname($name);
+$result = getname($name,$access_token);
 echo $result;
 
 
